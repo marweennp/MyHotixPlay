81b56e12ecc3b92f65340f7cc3aac14c4154a872
marwen <marwen.boudagga@esprit.tn>
2020-04-30 00:42:07 -0800

    Initial commit 30-04-2020

---
+++ app/src/main/java/com/hotix/myhotixplay/activites/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.hotix.myhotixplay.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hotix.myhotixplay.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
